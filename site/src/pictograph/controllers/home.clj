a5682ff88d38b9a3f9cae73d8f205653da2b9306
ryan spangler <ryan.spangler@gmail.com>
2013-03-21 13:59:41 -0700

    added previous and following slides for erudition

--- site/src/pictograph/controllers/home.clj
+++ site/src/pictograph/controllers/home.clj
@@ -19,8 +19,19 @@
 (defn slide
   [request]
   (let [slide-title (-> request :params :slide)
-        slide (model/pick
-               :slide
-               {:where {:title slide-title}})]
+        slide     (model/pick
+                   :slide
+                   {:where {:title slide-title}
+                    :include {:presentation {}}})
+        following (model/following
+                   :slide slide
+                   :presentation_position
+                   {:where (select-keys slide [:presentation_id])})
+        previous  (model/previous
+                   :slide slide
+                   :presentation_position
+                   {:where (select-keys slide [:presentation_id])})]
     (render (assoc request
-              :slide slide))))
+              :slide slide
+              :following following
+              :previous previous))))
